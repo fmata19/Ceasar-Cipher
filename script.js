2ba3b6ae77eb2f4667794596739278858e82f345
Francy Mata <francymata.fm@gmail.com>
2019-12-28 22:38:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const encryptBtn = document.querySelector('button');
+let msg;
+let shift;
+
+
+
+function inAlpha(letter){
+  const alpha=("abcdefghijklmnopqrstuvwxyz").split("");
+
+  if(alpha.indexOf(letter)== -1){
+    return false
+  }
+  else{
+    return true
+  }
+  
+}
+function getNewLetter(letter,shift){
+  const alpha=("abcdefghijklmnopqrstuvwxyz").split("");
+  let res;
+  currIndex= alpha.indexOf(letter);
+
+  if(currIndex+shift < 25){
+    res=alpha[currIndex+shift];
+  }else{
+    res=alpha[((currIndex+shift)-alpha.length)];
+  }
+
+  return res;
+}
+
+
+encryptBtn.onclick = function() {
+  msg = (document.getElementById("msg").value).split("");
+  shift= parseInt(document.getElementById("shift").value);
+
+  for(let i=0;i<msg.length;i++){
+    if(inAlpha(msg[i])){
+      msg[i]=getNewLetter(msg[i],shift);
+    }
+  }
+  document.getElementById("final").innerHTML="New Message: "+ msg.join("");
+}
+
+
+
+
+
+

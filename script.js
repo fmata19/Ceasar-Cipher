c7ea2fcabe8c4e9aab0f443a24017464b7267d20
Fmata <francymata.fm@gmail.com>
2019-12-30 22:29:20 -0500

    Updated OnClick Function

--- script.js
+++ script.js
@@ -1,56 +1,53 @@
-const encryptBtn = document.querySelector('button');
+const encryptBtn = document.getElementById('btn-1');
 let msg;
 let shift;
 
-
-
-function inAlpha(letter){
-  const alpha=("abcdefghijklmnopqrstuvwxyz").split("");
-
-  if(alpha.indexOf(letter)== -1){
-    return false
-  }
-  else{
-    return true
-  }
-  
-}
-function getNewLetter(letter,shift){
-  const alpha=("abcdefghijklmnopqrstuvwxyz").split("");
-  let res;
-  currIndex= alpha.indexOf(letter);
-
-  if(currIndex+shift < 25){
-    res=alpha[currIndex+shift];
-  }else{
-    res=alpha[((currIndex+shift)-alpha.length)];
-  }
-
-  return res;
-}
-
-
-encryptBtn.onclick = function() {
+encryptBtn.addEventListener("click",function () {
   msg = (document.getElementById("msg").value.toLowerCase()).split("");
-  shift= parseInt(document.getElementById("shift").value);
-  
-  if(msg !="" && shift!="" ){
-    for(let i=0;i<msg.length;i++){
-      if(inAlpha(msg[i])){
-        msg[i]=getNewLetter(msg[i],shift);
+  shift = parseInt(document.getElementById("shift").value);
+
+  if (msg != "" && shift != "") {
+    for (let i = 0; i < msg.length; i++) {
+      if (inAlpha(msg[i])) {
+        msg[i] = getNewLetter(msg[i], shift);
       }
     }
-    document.getElementById("final").innerHTML="New Message: "+ msg.join("");
+    document.getElementById("final").innerHTML = "New Message: " + msg.join("");
 
     document.getElementById("msg").value = "";
     document.getElementById("shift").value = "";
   }
-  else{
+  else {
     document.getElementById("msg").value = "Please Enter message..";
     document.getElementById("shift").value = "Please enter a number..";
   }
-}
+})
 
+
+function inAlpha(letter) {
+  const alpha = ("abcdefghijklmnopqrstuvwxyz").split("");
+
+  if (alpha.indexOf(letter) == -1) {
+    return false
+  }
+  else {
+    return true
+  }
+
+}
+function getNewLetter(letter, shift) {
+  const alpha = ("abcdefghijklmnopqrstuvwxyz").split("");
+  let res;
+  currIndex = alpha.indexOf(letter);
+
+  if (currIndex + shift < 25) {
+    res = alpha[currIndex + shift];
+  } else {
+    res = alpha[((currIndex + shift) - alpha.length)];
+  }
+
+  return res;
+}
 
 
 
